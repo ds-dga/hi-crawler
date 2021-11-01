2a369120e7e75fc5c3df78ac74c1a378de3fd95c
sipp11 <sipp11@zzyzx.co>
2021-11-01 15:23:59 +0700

    uptime bugfix

--- uptime_pusher.py
+++ uptime_pusher.py
@@ -10,7 +10,7 @@ def push(resp):
     _url = resp.url
     tm = resp.elapsed.total_seconds() * 100
     status_code = resp.status_code
-    _size = len(resp.content)
+    _size = len(resp.content) if resp.content else 0
     _from = socket.gethostname()
 
     body = {
@@ -20,23 +20,25 @@ def push(resp):
         "response_time_ms": tm,
         "from": _from,
     }
+    print(f"uptime hook: [{resp.status_code}] tm: {tm}, size: {_size}")
     resp = requests.post(
         uptime_uri,
         json=body,
     )
-    print(f"uptime hook: [{resp.status_code}] {resp.elapsed.total_seconds()} s")
+    print(f"uptime hook:[spice][{resp.status_code}] {resp.elapsed.total_seconds()} s//{resp.text}")
 
-def push_raw(id, status_code, resp_time):
+def push_raw(url, status_code, resp_time):
     _from = socket.gethostname()
     body = {
-        "id": id,
+        "url": url,
         "status_code": status_code,
         "size_byte": 0,
         "response_time_ms": resp_time,
         "from": _from,
     }
+    print(f"uptime hook raw: {status_code}] tm: {resp_time}")
     resp = requests.post(
         uptime_uri,
         json=body,
     )
-    print(f"uptime hook: [{resp.status_code}] {resp.elapsed.total_seconds()} s")
+    print(f"uptime hook raw:[spice][{resp.status_code}] {resp.elapsed.total_seconds()} s//{resp.text}")
